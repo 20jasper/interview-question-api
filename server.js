e16f1a4b9e03e4f490e9895fceb39abfa90e6393
Jacob Asper <jacobasper191@gmail.com>
2022-05-26 22:07:25 -0400

    added coinflip to server.js

--- server.js
+++ server.js
@@ -1,77 +1,63 @@
-const http = require('http');
+const http = require('http'); // module access
 const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
 const server = http.createServer((req, res) => {
+
+  const readWrite = (file, contentType) => {
+    fs.readFile(file, function (err, data) {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.write(data);
+      res.end();
+    });
+  }
+
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
+  switch (true) {
+    case page == '/':
+      readWrite('index.html', 'text/html');
+      break;
+    case page == '/otherpage':
+      readWrite('otherpage.html', 'text/html');
+      break;
+    case page == '/otherotherpage':
+      readWrite('otherotherpage.html', 'text/html');
+      break;
+    case page == '/api':
+      let flipResult = "type 'flip' in the input box"
+      if (params['student'] == 'flip') {
+        flipResult = Math.random() <= .5 ? 'heads' : 'tails'
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      const objToJson = {
+        name: flipResult,
+      }
+      res.end(JSON.stringify(objToJson));
+      break;
+    case page == '/css/style.css':
+      fs.readFile('css/style.css', function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case page == '/js/main.js':
+      readWrite('js/main.js', 'text/javascript');
+      break;
+    default:
+      figlet('404!!', function (err, data) {
+        if (err) {
           console.log('Something went wrong...');
           console.dir(err);
           return;
-      }
-      res.write(data);
-      res.end();
-    });
+        }
+        res.write(data);
+        res.end();
+      });
+      break;
   }
 });
 
