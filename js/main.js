f9f9bfe3ee26514cdc6d335972d8d9679f8918f7
Jacob Asper <jacobasper191@gmail.com>
2022-05-26 23:26:54 -0400

    removed console log

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ async function makeReq() {
   const res = await fetch(`/api?student`)
   const data = await res.json()
 
-  console.log(data);
   document.querySelector("#question").textContent = data.question
   document.querySelector("#questionType").textContent = data.questionType
 }
