4c1cf538b224f862acfc3c81ae7700f901b3798d
Jacob Asper <jacobasper191@gmail.com>
2022-05-26 23:49:35 -0400

    removed params from url

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq() {
 
-  const res = await fetch(`/api?student`)
+  const res = await fetch(`/api`)
   const data = await res.json()
 
   document.querySelector("#question").textContent = data.question
