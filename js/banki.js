1063208f04ebdad6fdd22cfbcbfcc11003ddce6a
Jacob Asper <jacobasper191@gmail.com>
2022-05-28 16:11:38 -0400

    added comment with more questions to the bottom

--- js/banki.js
+++ js/banki.js
@@ -886,10 +886,19 @@ export const banki = {
         "What is a Doubly Linked List",
       type: "CS Theory"
     },
-    {
-      question:
-        "What is a Doubly Linked List",
-      type: "CS Theory"
-    },
   ]
 };
+
+// "What is a Queue?"
+// "What is a Stack?""
+// "What is a Hash Table?"
+// "What is a Heap?"
+// "What is a Trie?"
+// "What is a Tree?"
+// "What is a Binary Search Tree?"
+// "What is a Disjoint Set?"
+// "What is a Bloom Filter?"
+// "Demonstrate Bubble Sort and explain when you might use it?"
+// "Demonstrate Insertion Sort and explain when you might use it?"
+// "Demonstrate Merge Sort and explain when you might use it?"
+// "Demonstrate Quicksort and explain when you might use it?"
